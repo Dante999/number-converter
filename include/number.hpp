f5d025d77c16fca0988520968ce149d7467fe915
Dante999 <dante999@users.noreply.github.com>
2020-09-17 23:32:47 +0200

    Restucture project tree

--- include/number.hpp
+++ include/number.hpp
@@ -9,7 +9,7 @@ class Number {
 
       private:
 	std::string m_hex;
-	std::string m_dec;
+	std::string m_decimal;
 	std::string m_ascii;
 	uint8_t     m_value;
 
@@ -22,30 +22,13 @@ class Number {
 	void from_decimal(const std::string &value);
 
       public:
-	Number(const std::string &input);
-	void set_value(const std::string &input);
+	Number(void);
+
+	void parse(const std::string &input);
 
 	const std::string_view get_hex(void) const;
 	const std::string_view get_decimal(void) const;
 	const std::string_view get_ascii(void) const;
 };
-
-void Number::to_hex(uint8_t value) {}
-
-void Number::to_ascii(uint8_t value) {}
-
-void Number::to_decimal(uint8_t value) {}
-
-void Number::from_hex(const std::string &value) {}
-
-void Number::from_ascii(const std::string &value) {}
-
-void Number::from_decimal(const std::string &value) {}
-
-const std::string_view Number::get_hex() const {}
-
-const std::string_view Number::get_decimal() const {}
-
-const std::string_view Number::get_ascii() const {}
 
 #endif // NUMBER_HPP_INCLUDED
--- src/number.cpp
+++ src/number.cpp
@@ -1,5 +1,12 @@
 #include "number.hpp"
 
+Number::Number() {}
+
+void Number::parse(const std::string &input)
+{
+	std::string prefix = input.substr(0, 2);
+}
+
 void Number::to_hex(uint8_t value) {}
 
 void Number::to_ascii(uint8_t value) {}
@@ -12,8 +19,17 @@ void Number::from_ascii(const std::string &value) {}
 
 void Number::from_decimal(const std::string &value) {}
 
-const std::string_view Number::get_hex() const {}
+const std::string_view Number::get_hex() const
+{
+	return m_hex;
+}
 
-const std::string_view Number::get_decimal() const {}
+const std::string_view Number::get_decimal() const
+{
+	return m_decimal;
+}
 
-const std::string_view Number::get_ascii() const {}
+const std::string_view Number::get_ascii() const
+{
+	return m_ascii;
+}
