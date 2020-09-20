7e5c03bd1d4678da9171b8ed37bc73d02bbc377d
Dante999 <dante999@users.noreply.github.com>
2020-09-20 16:40:09 +0200

    Add conan packet manager and gtest lib

---
+++ test/main_test.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+
+
+// Tests factorial of negative numbers.
+TEST(ExampleTest, WillPass) {
+  // This test is named "Negative", and belongs to the "FactorialTest"
+  // test case.
+  EXPECT_EQ(1, 1);
+
+}
+
+
